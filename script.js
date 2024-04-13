568ee01c09960d0fa47d09f9117f0ab660d5aa9d
Gustavolongen <71105380+Gustavolongensadlovski@users.noreply.github.com>
2024-04-13 15:58:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const box = document.getElementById('box');
+const topLeft = document.getElementById('topLeft');
+const topRight = document.getElementById('topRight');
+const bottomRight = document.getElementById('bottomRight');
+const bottomLeft = document.getElementById('bottomLeft');
+
+function updateBorderRadius() {
+    const radiusValue = `${topLeft.value}% ${topRight.value}% ${bottomRight.value}% ${bottomLeft.value}%`;
+    box.style.borderRadius = radiusValue;
+}
+
+topLeft.addEventListener('input', updateBorderRadius);
+topRight.addEventListener('input', updateBorderRadius);
+bottomRight.addEventListener('input', updateBorderRadius);
+bottomLeft.addEventListener('input', updateBorderRadius);
